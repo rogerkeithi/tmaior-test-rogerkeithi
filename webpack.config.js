da6b3dc3368f86b8d8e73a46139a6498a6f35458
rogerkeithi <nojiri.keithi@aluno.ifsp.edu.br>
2024-03-17 18:05:10 -0300

    feat: automatic messages

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,8 @@ const path = require('path');
 const fs = require('fs');
 const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin'); 
-const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  externals: [nodeExternals()],
   mode: 'production',
   entry: './server.js',
   output: {
